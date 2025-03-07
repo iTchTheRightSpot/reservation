104b61931f3c72da51e0de89d514f8f59fa7d47d
iTchTheRightSpot <emmanueluluabuike@gmail.com>
2025-03-06 19:03:13 -0500

    fixed logging date && log format

--- utils/log.go
+++ utils/log.go
@@ -19,44 +19,45 @@ type ILogger interface {
 	Fatal(variables ...interface{})
 }
 
+type logType string
+
 const (
-	iError = "ERROR"
-	iLog   = "LOG"
-	iFatal = "FATAL"
+	iError logType = "ERROR"
+	iLog   logType = "LOG"
+	iFatal logType = "FATAL"
 )
 
+type discord struct {
+	Status logType   `json:"status"`
+	Time   time.Time `json:"time"`
+	Info   string    `json:"info"`
+}
+
 type logger struct {
-	time     time.Time
 	location *time.Location
 	client   http.Client
 	webhook  string
 }
 
 func NewLogger(timezone, url string) (ILogger, error) {
-	if utc, loc, err := dateInTimezone(timezone); err != nil {
+	if loc, err := dateInTimezone(timezone); err != nil {
 		return nil, err
 	} else {
-		return &logger{time: utc, location: loc, client: http.Client{Timeout: 2 * time.Second}, webhook: url}, nil
+		return &logger{location: loc, client: http.Client{Timeout: 2 * time.Second}, webhook: url}, nil
 	}
 }
 
-func dateInTimezone(timezone string) (time.Time, *time.Location, error) {
+func dateInTimezone(timezone string) (*time.Location, error) {
 	if timezone == "" {
 		timezone = "America/Toronto"
 	}
 
 	location, err := time.LoadLocation(timezone)
 	if err != nil {
-		return time.Time{}, nil, err
+		return nil, err
 	}
 
-	dt, err := time.Parse(TimeFormat, time.Now().In(location).Format(TimeFormat))
-
-	if err != nil {
-		return time.Time{}, nil, err
-	}
-
-	return dt, location, nil
+	return location, nil
 }
 
 func (l *logger) post(mess string) {
@@ -64,14 +65,14 @@ func (l *logger) post(mess string) {
 		C string `json:"content"`
 	}
 
-	m, err := json.Marshal(&content{C: mess})
+	m, err := json.Marshal(&content{C: "@everyone\n" + mess})
 	if err != nil {
-		log.Printf("%s %s %v\n", l.time, iError, err.Error())
+		log.Printf("%s %s %v\n", l.Date(), iError, err.Error())
 		return
 	}
 
 	if _, err = http.Post(l.webhook, "application/json", bytes.NewBuffer(m)); err != nil {
-		log.Printf("%s %s %v\n", l.time, iError, err.Error())
+		log.Printf("%s %s %v\n", l.Date(), iError, err.Error())
 	}
 }
 
@@ -80,19 +81,56 @@ func (l *logger) Timezone() *time.Location {
 }
 
 func (l *logger) Date() time.Time {
-	return l.time
+	dt, err := time.Parse(TimeFormat, time.Now().In(l.location).Format(TimeFormat))
+	if err != nil {
+		log.Print(err.Error())
+	}
+	return dt
 }
 
 func (l *logger) Error(variables ...interface{}) {
-	log.Printf("%s %s %v\n", l.time, iError, variables)
-	l.post(fmt.Sprintf("## __**%s**__ on %s @everyone\n### %s", iError, l.time, variables))
+	obj := discord{
+		Status: iError,
+		Time:   l.Date(),
+		Info:   fmt.Sprintf("%v", variables),
+	}
+
+	js, err := json.MarshalIndent(obj, "", "  ")
+	if err != nil {
+		log.Printf("%s %s Failed to marshal log object: %v\n", l.Date(), iLog, err.Error())
+		return
+	}
+	log.Printf("%s\n", string(js))
+	l.post(string(js))
 }
 
 func (l *logger) Log(variables ...interface{}) {
-	log.Printf("%s %s %v\n", l.time, iLog, variables)
-	l.post(fmt.Sprintf("## __**%s**__ on %s @everyone\n### %s", iLog, l.time, variables))
+	obj := discord{
+		Status: iLog,
+		Time:   l.Date(),
+		Info:   fmt.Sprintf("%v", variables),
+	}
+
+	js, err := json.MarshalIndent(obj, "", "  ")
+	if err != nil {
+		log.Printf("%s %s Failed to marshal log object: %v\n", l.Date(), iLog, err.Error())
+		return
+	}
+	log.Printf("%s\n", string(js))
+	l.post(string(js))
 }
 
 func (l *logger) Fatal(variables ...interface{}) {
-	log.Fatalf("%s %s %v\n", l.time, iFatal, variables)
+	obj := discord{
+		Status: iFatal,
+		Time:   l.Date(),
+		Info:   fmt.Sprintf("%v", variables),
+	}
+
+	js, err := json.MarshalIndent(obj, "", "  ")
+	if err != nil {
+		log.Printf("%s %s Failed to marshal log object: %v\n", l.Date(), iLog, err.Error())
+		return
+	}
+	log.Printf("%s\n", string(js))
 }
