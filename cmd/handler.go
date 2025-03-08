6f598d1ef42ca7d481103fa39d3bbc94f8603d4c
iTchTheRightSpot <emmanueluluabuike@gmail.com>
2025-03-08 12:42:49 -0500

    clean up

--- cmd/handler.go
+++ cmd/handler.go
@@ -17,31 +17,25 @@ type HandlerRegistry struct {
 	lg   utils.ILogger
 	env  *config.SecretVariables
 	mux  *http.ServeMux
-	ss   *serviceRegistry
+	sr   *serviceRegistry
 	ware *middleware.Middleware
 	f    http.FileSystem
 }
 
 func NewHandlerRegistry(db *sql.DB, l utils.ILogger, e *config.SecretVariables, f http.FileSystem) *HandlerRegistry {
 	s := newServiceRegistry(db, l, e)
-	return &HandlerRegistry{
-		f:    f,
-		lg:   l,
-		env:  e,
-		mux:  http.NewServeMux(),
-		ss:   s,
-		ware: &middleware.Middleware{Logger: l, Auth: s.JwtService, Param: e.CookieParam, ApiPrefix: e.ApiPrefix, FileSystem: f},
-	}
+	ware := middleware.Middleware{Logger: l, Auth: s.JwtService, Param: e.CookieParam, ApiPrefix: e.ApiPrefix, FileSystem: f}
+	return &HandlerRegistry{f: f, lg: l, env: e, mux: http.NewServeMux(), sr: s, ware: &ware}
 }
 
 func (dep *HandlerRegistry) Initialize() http.Handler {
 	v1 := http.NewServeMux()
 
-	schedule.NewScheduleHandler(v1, dep.ware, dep.lg, dep.ss.ScheduleService).Register()
-	service_type.NewServiceTypeHandler(v1, dep.lg, dep.ss.ServiceImpl, dep.ware).Register()
-	staff.NewStaffHandler(v1, dep.ware, dep.lg, dep.ss.StaffService).Register()
-	reservation.NewReservationHandler(v1, dep.lg, dep.ware, dep.ss.ReservationService).Register()
-	account.NewAccountHandler(v1, dep.ware, dep.lg, dep.env, dep.ss.PasswordService, dep.ss.AccountService).Register()
+	schedule.NewScheduleHandler(v1, dep.ware, dep.lg, dep.sr.ScheduleService).Register()
+	service_type.NewServiceTypeHandler(v1, dep.lg, dep.sr.ServiceImpl, dep.ware).Register()
+	staff.NewStaffHandler(v1, dep.ware, dep.lg, dep.sr.StaffService).Register()
+	reservation.NewReservationHandler(v1, dep.lg, dep.ware, dep.sr.ReservationService).Register()
+	account.NewAccountHandler(v1, dep.ware, dep.lg, dep.env, dep.sr.PasswordService, dep.sr.AccountService).Register()
 
 	dep.mux.Handle(dep.env.ApiPrefix, http.StripPrefix(dep.env.ApiPrefix[:len(dep.env.ApiPrefix)-1], v1))
 
